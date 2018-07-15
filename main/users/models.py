df936f0ed62a8fec1d15c788c98edc762a51a608
lgants <lgants@gmail.com>
2018-07-15 18:40:42 -0500

    updated user models

--- main/users/models.py
+++ main/users/models.py
@@ -5,11 +5,42 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     username = models.CharField(max_length=256, unique=True)
     email = models.CharField(max_length=128, unique=True)
+    role = models.CharField(max_length=128, default='user')
+    is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user_profile = models.ForeignKey('users.UserProfile', blank=True, null=True, on_delete=models.CASCADE)
 
 
 class UserProfile(models.Model):
-    first_name = models.CharField(max_length=128, unique=True)
-    last_name = models.CharField(max_length=128, unique=True)
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+
+class AuthCertificate(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    serial = models.CharField(max_length=128, unique=True)
+
+
+class AuthFacebook(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    fb_id = models.CharField(max_length=256, unique=True)
+    display_name = models.CharField(max_length=256)
+
+
+class AuthGoogle(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    google_id = models.CharField(max_length=256, unique=True)
+    display_name = models.CharField(max_length=256)
+
+
+class AuthGithub(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    gh_id = models.CharField(max_length=256, unique=True)
+    display_name = models.CharField(max_length=256)
+
+
+class AuthLinkedin(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    ln_id = models.CharField(max_length=256, unique=True)
+    display_name = models.CharField(max_length=256)
